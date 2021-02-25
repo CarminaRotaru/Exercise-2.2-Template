4c79e0b9ef4398ab11400bf1738d70bd54aebd56
CarminaRotaru <60619555+CarminaRotaru@users.noreply.github.com>
2021-02-24 19:01:14 -0500

    Update Response.js

--- src/router/Response.js
+++ src/router/Response.js
@@ -2,7 +2,37 @@
  * This class is responsible for getting the data ready to go back to the client.
  */
 class Response {
+    constructor(statusCode = 200, message = "", payload = {}){
+        this.statusCode = statusCode;
+        this.message = message;
+        this.payload = payload;
+    }
 
+    getStatusCode(){
+        return this.statusCode;
+    }
+
+    getMessage(){
+        return this.message;
+    }
+
+    getPayload(){
+        return this.payload;
+    }
+
+    setResponse(response){
+        this.statusCode = response.statusCode;
+        this.message = response.message;
+        this.payload = response.payload;
+    }
+
+    toString(){
+        return '{"statusCode":' + this.statusCode + ',"message\":\"' + this.message + '\","payload":' + this.payLoadToString() +'}';
+    }
+
+    payLoadToString(){
+        return '{\"id\":' + this.payload.id + ',\"name\":\"' + this.payload.name + '\",\"type\":\"' + this.payload.type + '\"}';
+    }
 }
 
 module.exports = Response;
